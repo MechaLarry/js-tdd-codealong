73e0db23b6264364bdb0c5ff48c2f84968eae318
MechaLarry <mecha.larry@gmail.com>
2024-08-16 00:13:51 +0300

    my first commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 // Your code here
+// utils.js
+
+export function currentAgeForBirthYear(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+  }
+  
+  export default currentAgeForBirthYear;
